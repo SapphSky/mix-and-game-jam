02379782d114166422f35012be64c1586e404b5a
Aaron <aaronmirotta@gmail.com>
2019-10-12 14:34:10 -0400

    Added enemy controller and enemy stand in
    enemy will move randomly until the player is within a radius. then the enemy will chase the player

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Rigidbody enemy;
+
+    [Header("Random movement")]
+    public float maxRandRangeX = 0;
+    public float minRandRangeX = 0;
+    public float maxRandRangeY = 0;
+    public float minRandRangeY = 0;
+    public float movePause = 0;
+    public float randMoveTimer = 0;
+
+
+    [Header("Health")]
+    public float health;
+    [Header("Movement")]
+    public float movementSpeed;
+    [Header("Attack")]
+    public float attackSpeed;
+    public bool playerDetect = false;
+
+    
+
+    private void Update()
+    {
+        if (!playerDetect)
+        {
+
+            RandomMovement();
+        }
+        else
+        {
+            
+        }
+        
+    }
+
+    private void RandomMovement()
+    {
+        Vector2 movementDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
+        Vector2 movementPerSecond = movementDirection * movementSpeed;
+        //create random Vector2
+        Vector2 moveRandom = new Vector2(Random.Range(minRandRangeX,maxRandRangeX),Random.Range(minRandRangeY,maxRandRangeY));
+        //set the timer
+        randMoveTimer= randMoveTimer - 1 * Time.deltaTime;
+        if(randMoveTimer <= 0)
+        {
+            randMoveTimer = movePause;
+
+            enemy.velocity = moveRandom;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        playerDetect = true;
+
+        if(other.gameObject.tag == "Player")
+        {
+            Vector2 playerPos = new Vector2(other.transform.position.x - transform.position.x, other.transform.position.y - transform.position.y);
+
+            enemy.velocity = playerPos;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        playerDetect = false;
+    }
+}
