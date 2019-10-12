859d83555c21be531c254dad7f680a06963f2fde
SapphSky <44979895+SapphSky@users.noreply.github.com>
2019-10-12 10:36:53 -0400

    Project Initialization

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJam {
+	public class Character : MonoBehaviour {
+		public int health;
+		public int maxHealth;
+
+		public delegate void OnKilled();
+		public event OnKilled Killed;
+	}
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJam {
+	public class CharacterController : MonoBehaviour {
+		public struct Inputs {
+			public string horizontal;
+			public string vertical;
+			public string jump;
+
+			public string vacuum;
+			public string blower;
+		}
+		public Inputs inputs;
+
+		public Transform aimTransform;
+
+		private Camera cam;
+
+		private void Start() {
+			cam = Camera.main;
+		}
+
+		private void Update() {
+			Vector3 point = new Vector3();
+			Event currentEvent = Event.current;
+			Vector2 mousePos = new Vector2();
+
+			mousePos.x = currentEvent.mousePosition.x;
+			mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
+
+			point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+
+			aimTransform.LookAt(point, Vector3.up);
+		}
+	}
+}
