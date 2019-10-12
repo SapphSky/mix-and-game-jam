d21e67f9283af345b19b8c36d4573a0a75d8fbea
SapphSky <44979895+SapphSky@users.noreply.github.com>
2019-10-12 16:54:51 -0400

    Character Health Events, Vacuum Ability

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,10 +4,54 @@ using UnityEngine;
 
 namespace GameJam {
 	public class Character : MonoBehaviour {
-		public int health;
-		public int maxHealth;
+		[Header("Parameters")]
+		public int CurrentHealth;
+		public int MaxHealth;
+		public bool canTakeDamage;
+
+		// Events
+		public delegate void OnHealed();
+		public event OnHealed Healed;
+
+		public delegate void OnDamage();
+		public event OnDamage Damaged;
 
 		public delegate void OnKilled();
 		public event OnKilled Killed;
+
+		private void Awake() {
+			
+		}
+
+		private void Start() {
+			
+		}
+
+		private void Update() {
+			if (CurrentHealth <= 0) {
+				// Game Over
+			}
+		}
+
+		private void TakeDamage(int amount) {
+			if (canTakeDamage) {
+				CurrentHealth -= amount;
+				Damaged();
+				StartCoroutine(IFrames(2.0f));
+			}
+		}
+
+		private void Heal(int amount) {
+			CurrentHealth += amount;
+			Healed();
+		}
+
+		private IEnumerator IFrames(float duration) {
+			canTakeDamage = false;
+			// Perform IFrame indication here?
+
+			yield return new WaitForSeconds(duration);
+			canTakeDamage = true;
+		}
 	}
 }
--- Assets/Scripts/Vacuum.cs
+++ Assets/Scripts/Vacuum.cs
@@ -5,11 +5,46 @@ using UnityEngine;
 namespace GameJam {
 	public class Vacuum : MonoBehaviour {
 		public bool active;
+		public bool hasObjectHeld;
+		public Rigidbody2D heldObject;
+
+		private Rigidbody2D body;
+
+		private void Awake() {
+			body = GetComponent<Rigidbody2D>();
+		}
 
 		private void Update() {
 			if (active) {
-				Debug.DrawRay(transform.position, transform.forward, Color.white);
+				Primary();
 			}
+		}
+
+		private void Primary() {
+			Debug.DrawLine(body.position, body.transform.right * 3.0f);
+			RaycastHit2D[] hits = Physics2D.CapsuleCastAll(transform.position, new Vector2(0.5f, 3.0f), CapsuleDirection2D.Vertical, 0, Vector2.right);
+			if (hits.Length > 0) {
+				foreach (var item in hits) {
+					if (item.transform.GetComponent<Rigidbody2D>()) {
+						PullTowards(item.transform.GetComponent<Rigidbody2D>());
+					}
+				}
+			}
+		}
+
+		private void PullTowards(Rigidbody2D obj) {
+			var distToPoint = obj.position - body.position;
+			obj.AddForce(-distToPoint, ForceMode2D.Force);
+		}
+
+		private void HoldObject(Rigidbody2D obj) {
+			var joint = obj.gameObject.AddComponent<FixedJoint2D>();
+			joint.connectedBody = GetComponent<Rigidbody2D>();
+		}
+
+		private void ReleaseObject(Rigidbody2D obj) {
+			Destroy(obj.gameObject.GetComponent<FixedJoint2D>());
+			obj.AddForce(transform.right * 5, ForceMode2D.Impulse);
 		}
 	}
 }
