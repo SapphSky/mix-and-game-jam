2c633b23b68a2bcb35b8bf3e41cc188395ef038d
Aaron <aaronmirotta@gmail.com>
2019-10-12 17:24:19 -0400

    Added smooth camera movement following the player

---
+++ Assets/Scripts/SmoothCameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour
+{
+    private Vector2 velocity;
+
+    public float smoothTimeY;
+    public float smoothTimeX;
+
+    public GameObject Player;
+
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+    private void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, Player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, Player.transform.position.y, ref velocity.y, smoothTimeY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+}
