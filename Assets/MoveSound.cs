8bb44d20c50195d0662376c3bf98a0fa54c66a99
ockenyberg <ocke.nyberg@gmail.com>
2019-10-13 15:46:37 +0200

    Added furniture moving sound. It only starts, and I can't get it to stop and restart properly.

---
+++ Assets/MoveSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSound : MonoBehaviour
+{
+  bool didItPlay = false;
+  public AK.Wwise.Event MovingSound = null;
+      
+  
+      void Update () 
+      {
+          if(GetComponent<Rigidbody2D>().velocity.magnitude >= 0.1 && !didItPlay)
+          {
+              MovingSound.Post(gameObject);
+              didItPlay = true;
+          }
+          else
+          {
+             // MovingSound.Stop(gameObject);
+             // didItPlay = false;
+          }
+
+
+      }
+}
+//!MovingSound.Post
